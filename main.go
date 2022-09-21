82d60ebb43792bcdbe47c578bcb7515d33aa6e32
vernik03 <vernik03@gmail.com>
2022-09-21 15:24:51 +0300

    add semaphore

--- main.go
+++ main.go
@@ -1,68 +1,84 @@
 package main
 
 import (
-	// "math/rand"
-	// "sync"
-	// "fmt"
-	// "time"
+	"errors"
+	"fmt"
+	"os"
+	"time"
 )
 
-// type monk struct {
-//     monastery string
-//     energy  int
-// }
+var (
+	ErrNoTickets      = errors.New("semaphore: could not aquire semaphore")
+	ErrIllegalRelease = errors.New("semaphore: can't release the semaphore without acquiring it first")
+)
 
-// func fight(monk1 *monk, monk2 *monk) int {
-// 	time.Sleep(time.Millisecond * 300)
-//     if monk1.energy > monk2.energy {
-// 		fmt.Println(monk1.monastery + " wins")
-// 		return 1
-// 	} else {
-// 		fmt.Println(monk2.monastery + " wins")
-// 		return 2
-// 	}
-// }
+// Interface содержит поведение семафора, который может быть захвачен (Acquire) и/или освобожден (Release).
+type Interface interface {
+	Acquire() error
+	Release() error
+}
+
+type implementation struct {
+	sem     chan struct{}
+	timeout time.Duration
+}
+
+func (s *implementation) Acquire() error {
+	select {
+	case s.sem <- struct{}{}:
+		return nil
+	case <-time.After(s.timeout):
+		return ErrNoTickets
+	}
+}
+
+func (s *implementation) Release() error {
+	select {
+	case _ = <-s.sem:
+		return nil
+	case <-time.After(s.timeout):
+		return ErrIllegalRelease
+	}
+}
+
+func New(tickets int, timeout time.Duration) Interface {
+	return &implementation{
+		sem:     make(chan struct{}, tickets),
+		timeout: timeout,
+	}
+}
+
+struct Smoker_paper {
+	int paper = 1000;
+	int tobacco = 0;
+	int matches = 0;
+}
+
+struct Smoker_tobacco {
+	int paper = 0;
+	int tobacco = 1000;
+	int matches = 0;
+}
+
+struct Smoker_matches {
+	int paper = 0;
+	int tobacco = 0;
+	int matches = 1000;
+}
 
 func main() {
-	// rand.Seed(time.Now().UnixNano())
-	// Guan_Yin := []monk{}
-	// Guan_Yan := []monk{}
-	// for i := 0; i < 20; i++ {
-	// 	Guan_Yin = append(Guan_Yin, monk{"Guan-Yin", rand.Intn(90)+10})
-	// 	Guan_Yan = append(Guan_Yan, monk{"Guan-Yan", rand.Intn(90)+10})
-	// }
+	fmt.Println("Начало")
+	tickets, timeout := 0, 0*time.Second
+	s := New(tickets, timeout)
+	
 
-	// fighters := []monk{}
-	// for i := 0; i < len(Guan_Yin); i++ {
-	// 	fighters = append(fighters, Guan_Yin[i])
-	// 	fighters = append(fighters, Guan_Yan[i])
-	// }
+	if err := s.Acquire(); err != nil {
+		if err != ErrNoTickets {
+			panic(err)
+		}
 
-	// for len(fighters) > 2 {
-	// 	// fmt.Println(fighters)
-	// 	var wg sync.WaitGroup 
-	// 	wg.Add(len(fighters)/2)
-	// 	for i := 0; i < len(fighters)-1; i += 2 {
-	// 		go func(monk1 *monk, monk2 *monk) {
-	// 			defer wg.Done()
-	// 			winner := fight(monk1, monk2)
-	// 			if winner == 1 {
-	// 				monk1.energy = 0
-	// 			} else {
-	// 				monk2.energy = 0
-	// 			}
-	// 		  }(&fighters[i], &fighters[i+1])
-	// 	}
-	// 	wg.Wait()
-	// 	temp_fighters := []monk{}
-	// 	for i := 0; i < len(fighters); i++ {
-	// 		if fighters[i].energy > 0 {
-	// 			temp_fighters = append(temp_fighters, fighters[i])
-	// 		}
-	// 	}
-	// 	fighters = temp_fighters
-	// 	fmt.Println("")
-	// }
-
-	// fmt.Println("Finally, " + fighters[0].monastery + " wins")
+		fmt.Println("Билетов не осталось, не могу работать")
+		os.Exit(1)
+	}
+	fmt.Println("Завершение")
 }
